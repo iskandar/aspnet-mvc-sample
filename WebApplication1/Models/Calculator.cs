ba328e1e9a0049b2f872c7ce7deecfe81d03aec3
Iskandar Najmuddin <iskandar.najmuddin@rackspace.co.uk>
2016-02-27 12:04:40 +0000

    Add NUnit tests

---
+++ WebApplication1/Models/Calculator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public interface ICalculator
+    {
+        int Add(int num1, int num2);
+        int Mul(int num1, int num2);
+    }
+
+    public class Calculator : ICalculator
+    {
+
+        public int Add(int num1, int num2)
+        {
+            int result = num1 + num2;
+            return result;
+        }
+
+        public int Mul(int num1, int num2)
+        {
+            int result = num1 * num2 + 1;
+            return result;
+        }
+    }
+}
---
+++ WebApplication1/Models/CalculatorTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using NUnit.Framework;
+
+
+namespace WebApplication1.Models
+{
+    [TestFixture]
+    public class CalculatorTest
+    {
+        [Test]
+        public void ShouldAddTwoNumbers()
+        {
+            ICalculator sut = new Calculator();
+            int expectedResult = sut.Add(7, 8);
+            Assert.That(expectedResult, Is.EqualTo(15));
+        }
+
+        [Test]
+        public void ShouldMulTwoNumbers()
+        {
+            ICalculator sut = new Calculator();
+            int expectedResult = sut.Mul(7, 8);
+            Assert.That(expectedResult, Is.EqualTo(56));
+        }
+
+    }
+}
