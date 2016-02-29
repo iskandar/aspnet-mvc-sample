c2307fcd0288f7d006bd2c991846533b71f9e171
Iskandar Najmuddin <iskandar.najmuddin@rackspace.co.uk>
2016-02-29 14:28:39 +0000

    Revert "Introduce Calculator bug"
    This reverts commit 38a7b8c3b6adcec392ca86f9c6a777c4283184e0.

--- WebApplication1/Models/Calculator.cs
+++ WebApplication1/Models/Calculator.cs
@@ -22,8 +22,7 @@ namespace WebApplication1.Models
 
         public int Mul(int num1, int num2)
         {
-            int result = num1 * num2 + 1;
-//            int result = num1 * num2;
+            int result = num1 * num2;
             return result;
         }
     }
