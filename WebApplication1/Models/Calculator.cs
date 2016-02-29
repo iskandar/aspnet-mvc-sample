843d434e1427f92619b3038d7f8699a777720070
Iskandar Najmuddin <iskandar.najmuddin@rackspace.co.uk>
2016-02-29 15:43:04 +0000

    Break calc Mul function

--- WebApplication1/Models/Calculator.cs
+++ WebApplication1/Models/Calculator.cs
@@ -23,13 +23,14 @@ namespace WebApplication1.Models
 
         public int Mul(int num1, int num2)
         {
-            int result = num1 * num2;
+//            int result = num1 * num2;
+            int result = num1 * num2 + 1;
             return result;
         }
 
         public int Sq(int num1)
         {
-            int result = num1 * num1 + 2;
+            int result = num1 * num1 + 3;
 //            int result = num1 * num1;
             return result;
         }
