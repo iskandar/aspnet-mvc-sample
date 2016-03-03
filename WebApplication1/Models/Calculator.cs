665ac1fcb76ce3cc29662457e69def447740aa53
Iskandar Najmuddin <iskandar.najmuddin@rackspace.co.uk>
2016-03-03 17:19:15 +0000

    FIx tests

--- WebApplication1/Models/Calculator.cs
+++ WebApplication1/Models/Calculator.cs
@@ -23,15 +23,15 @@ namespace WebApplication1.Models
 
         public int Mul(int num1, int num2)
         {
-//            int result = num1 * num2;
-            int result = num1 * num2 + 1;
+            int result = num1 * num2;
+//            int result = num1 * num2 + 1;
             return result;
         }
 
         public int Sq(int num1)
         {
-            int result = num1 * num1 + 3;
-//            int result = num1 * num1;
+//            int result = num1 * num1 + 3;
+            int result = num1 * num1;
             return result;
         }
     }
